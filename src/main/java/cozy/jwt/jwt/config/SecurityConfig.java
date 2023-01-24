c32a5271647ee70c47776b1592e18965c6edf659
yongdev <yong50907845@gmail.com>
2023-01-24 14:28:00 +0900

    jwt project start

---
+++ src/main/java/cozy/jwt/jwt/config/SecurityConfig.java
@@ -0,0 +1,16 @@
+package cozy.jwt.jwt.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig  extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests().antMatchers("/api/hello").permitAll()
+                .anyRequest().authenticated();
+    }
+
+}
